8936726aa251561415ee532e37b09c001ebe4702
Gonçalo Alegria <goncaloantonioalegria16@gmail.com>
2023-06-06 22:35:22 +0100

    Multi server bot and event commands created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+const { Token } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -23,8 +23,20 @@ for (const folder of commandFolders) {
 		}
 	}
 }
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-client.once(Events.ClientReady, () => {
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(client,...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(client,...args));
+	}
+}
+
+/* client.once(Events.ClientReady, () => {
 	console.log('Ready!');
 });
 
@@ -48,5 +60,5 @@ client.on(Events.InteractionCreate, async interaction => {
 		}
 	}
 });
-
-client.login(token);
+ */
+client.login(Token);
