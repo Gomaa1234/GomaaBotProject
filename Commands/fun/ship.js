88ceb3b49a90fef84757e1544c7afbd4e88ffa55
Gonçalo Alegria <goncaloantonioalegria16@gmail.com>
2023-06-09 21:16:47 +0100

    mission failed

--- Commands/fun/ship.js
+++ Commands/fun/ship.js
@@ -2,7 +2,6 @@ const { SlashCommandBuilder } = require('discord.js');
 const request = require('request');
 const fs = require('fs');
 const sharp = require('sharp');
-const { open, rm, unlink } = require('fs/promises');
 const wait = require('node:timers/promises').setTimeout;
 const download = (url, path, callback) => {
 	request.head(url, () => {
@@ -11,17 +10,9 @@ const download = (url, path, callback) => {
 			.on('close', callback);
 	});
 };
-
-async function del(file,filePath){
-	try {
-		rm(filePath);
-	}catch (error) {
-		console.log(error);
-	}
-}
 async function compositeImages(img1, img2, backImg, t) {
 	try {
-	await sharp(backImg)
+		await sharp(backImg)
 		.composite([
 			{
 				input: img1,
@@ -34,9 +25,9 @@ async function compositeImages(img1, img2, backImg, t) {
 				left: 256,
 			},
 		])
-		.toFile("./img-temp/out.jpg",  async() =>{
-			const end3 = Date.now();
-			console.log(`Execution time: ${end3 - t} ms ./img-temp/out.jpg`);
+		.toFile("./img-temp/out.jpg", () =>{
+			const end = Date.now();
+			console.log(`Execution time: ${end - t} ms ./img-temp/out.jpg`);
 		})
 	} catch (error) {
 	  console.log(error);
@@ -49,33 +40,38 @@ module.exports = {
 		.addUserOption(option1 => option1.setName('target1').setDescription('The user\'s avatar to show').setRequired(true))
 		.addUserOption(option2 => option2.setName('target2').setDescription('The user\'s avatar to show')),
 	async execute(interaction) {
-		
+		let start = null;
 		const user1 = interaction.options.getUser('target1');
 		const user2 = interaction.options.getUser('target2');
-		const urlImgUser1 = `${user1.displayAvatarURL({ dynamic: true, format: 'jpg', size: 256 })}`;
+		let urlImgUser1 = `${user1.displayAvatarURL({ dynamic: true, format: 'jpg', size: 256 })}`;
+		if(!fs.existsSync(`./img-temp/${interaction.guild.id}temp`))
+			fs.mkdirSync(`./img-temp/${interaction.guild.id}temp`)
 		let urlImgUser2 = null;
-		const pathImgUser1 = `./img-temp/${user1.id}temp.jpg`;
+		let pathImgUser1 = `./img-temp/${interaction.guild.id}temp/${user1.id}temp.jpg`;
 		let pathImgUser2 = null;
 		if(!user2){
-
 		}else{
 			urlImgUser2 = `${user2.displayAvatarURL({ dynamic: true, format: 'jpg', size: 256 })}`;
-			pathImgUser2 = `./img-temp/${user2.id}temp.jpg`
+			pathImgUser2 = `./img-temp/${interaction.guild.id}temp/${user2.id}temp.jpg`
 		}
-		const start1 = Date.now();
-		await download (urlImgUser1, pathImgUser1, () =>{
-			console.log("✅ Done1");
-			const end1 = Date.now();
-			console.log(`Execution time: ${end1 - start1} ms ${pathImgUser1}`);
-		});
-		const start2 = Date.now();
-		await download (urlImgUser2, pathImgUser2, () =>{
-			console.log("✅ Done2");
-			const end2 = Date.now();
-			console.log(`Execution time: ${end2 - start2} ms ${pathImgUser2}`);
-		});
-		await wait(1000);
-		const start3 = Date.now();
-		await compositeImages(pathImgUser1,pathImgUser2,'./img-temp/back-img.png',start3);
+		start = Date.now();
+		if(!fs.existsSync(pathImgUser1)){
+			await download (urlImgUser1, pathImgUser1, () =>{
+				console.log("✅ Done1");
+				const end = Date.now();
+				console.log(`Execution time: ${end - start} ms ${pathImgUser1}`);
+			});
+		}
+		start = Date.now();
+		if(!fs.existsSync(pathImgUser1)){
+			await download (urlImgUser2, pathImgUser2, () =>{
+				console.log("✅ Done2");
+				const end = Date.now();
+				console.log(`Execution time: ${end - start} ms ${pathImgUser2}`);
+			});
+		}
+		await wait(500)
+		start = Date.now();
+		await compositeImages(pathImgUser1,pathImgUser2,'./img-temp/back-img.png',start);
 	},
 };
