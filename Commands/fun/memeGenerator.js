a12147cbcfa41ffb0905a6afa386447475c17fc8
Gonçalo Alegria <goncaloantonioalegria16@gmail.com>
2023-07-05 16:55:44 +0100

    Momo Gen work and new module

--- Commands/fun/memeGenerator.js
+++ Commands/fun/memeGenerator.js
@@ -1,12 +1,19 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder  } = require('discord.js')
 const message = require('../../EmbedMessages/Message.js')
 const error = require('../../EmbedMessages/Error.js')
+const fetch = require('node-fetch')
 module.exports = {
 	data: new SlashCommandBuilder()
 	// info about the command like Name, Description, etc.
 		.setName('memegen')
 		.setDescription('Sends a random meme'),
 	async execute(interaction) {
-
+		let url = "https://meme-api.com/gimme";
+		let settings = { method: "Get" };
+		fetch(url, settings)
+			.then(res => res.json())
+			.then((json) => {
+				interaction.reply(json.url)
+		});
 	},
 };
