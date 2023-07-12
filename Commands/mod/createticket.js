95ac5f4497c77d8e27871db8300d8ee43292c1a5
Gonçalo Alegria <goncaloantonioalegria16@gmail.com>
2023-07-12 13:02:10 +0100

    removed unsesesary code

--- Commands/mod/createticket.js
+++ Commands/mod/createticket.js
@@ -4,9 +4,6 @@ const error = require('../../EmbedMessages/Error.js')
 const JSONPath = "../../Servers"
 const fs = require('fs');
 const path = require('node:path');
-var split = function (str) {
-    return str.split('\\').pop().split('/').pop();
-}
 module.exports = {
 	data: new SlashCommandBuilder()
 	// info about the command like Name, Description, etc.
