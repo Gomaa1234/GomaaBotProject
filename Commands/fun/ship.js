95839686ca44fc23aad2d36cfce443ca447f37b3
Gonçalo Alegria <goncaloantonioalegria16@gmail.com>
2023-07-20 20:54:47 +0100

    optimize ship command

--- Commands/fun/ship.js
+++ Commands/fun/ship.js
@@ -2,125 +2,123 @@ const { SlashCommandBuilder, EmbedBuilder, AttachmentBuilder} = require('discord
 const axios = require('axios');
 const fs = require('fs');
 const sharp = require('sharp');
-const wait = require('node:timers/promises').setTimeout;
 const { PositionShip } = require('../../imagePos.json');
 const error = require('../../EmbedMessages/Error.js')
 // this converts url to a base 64
-async function URLimg(url){
-	// get image Buffer from url and convert to Base64
-    const image = await axios.get(url, {responseType: 'arraybuffer'});
-    const base64Image = Buffer.from(image.data).toString('base64');
+async function URLimg(url) {
+	// Get the image as a Buffer.
+	const imageBuffer = await axios.get(url, {responseType: 'arraybuffer'});
+  
+	// Convert the Buffer to Base64.
+	const base64Image = Buffer.from(imageBuffer).toString('base64');
+  
+	// Return the Base64 image.
 	return base64Image;
-}
+  }
 // this function return the index to a array  of color
-function getColorId(colorArray, num){
-	// this gets the percentage of array length divided by 100
-	let valPercentage = 100/colorArray.length;
-	// this variable serves do add percentage gradually.
+const getColorId = (colorArray, num) => {
+	// Get the ceiling of the percentage.
+	const valPercentage = Math.ceil(100 / colorArray.length);
+	// Initialize the current value.
 	let valConst = 0;
-	for(i = 0; i<colorArray.length; i++){
-		// see is num is bigger than valConst plus valPercentage
-		if(num > valConst+valPercentage){
-			// adds valPercentage to valConst
-			valConst += valPercentage;
-		}
-		else{
-			// return index when false
-			return i
-		}
-	} 
-}
-async function compositeImages(img1, img2, backImg, dir, t, num, interaction,user1, user2) {
-	try {
-		// random number 0 to number of images template.
-		const path = `./img/img-temple/ship/`
-		let randomInt = null
-		fs.readdir(path, (err, files) => {
-			randomInt = Math.floor(Math.random() * files.length);
-		});
-		await wait(10)
-		const width = 1024;
-		const height = 512;
-		const color = ["#FF0D0D", "#FF4E11", "#FF8E15", "#FAB733", "#ACB334", "#69B34C","#33CC33"];
-		// gets color depending of the variable num.
-		const i = getColorId(color,num)
-		// this create a svg image with text on it with color and variable num.
-		const svgImage = `
-			<svg width="${width}" height="${height}">
-				<style>
-				.title { 
-					fill: ${color[i]};
-					font-size: 100px; 
-					font-family: Arial, sans-serif; 
-					font-weight: normal;
-					paint-order: stroke;
-					stroke: #000000;
-					stroke-width: 8px;
-					stroke-linecap: butt;
-					stroke-linejoin: miter;
-				}
-				</style>
-				<text x="50%" y="95%" text-anchor="middle" class="title">${num}%</text>
-			</svg>
-		`;
-		// this create a buffer to from the svgImage, img1Buffer and img2Buffer.
-		const svgBuffer = Buffer.from(svgImage);
-		let img1Buffer = Buffer.from(img1, 'base64');
-		let img2Buffer = Buffer.from(img2, 'base64');
-		// this adjust the image size to the PositionShip[index].width.
-		img1Buffer = await sharp(img1Buffer)
-			.resize({ width: PositionShip[randomInt].width })
-			.toBuffer()
-		img2Buffer = await sharp(img2Buffer)
-			.resize({ width: PositionShip[randomInt].width })
-			.toBuffer()
-		// this composite all images in the next order 1st img1Buffer, 2nd img2Buffer, 3rd image template and 4th svgBuffer .
-		sharp(backImg)
-			.composite([
-				{
-					input: img1Buffer,
-					top: PositionShip[randomInt].y1,
-					left: PositionShip[randomInt].x1,
-				},
-				{
-					input: img2Buffer,
-					top: PositionShip[randomInt].y2,
-					left: PositionShip[randomInt].x2,
-				},
-				{
-					input: `./img/img-temple/ship/${randomInt + 1}.png`,
-					top: 0,
-					left: 0,
-				},
-				{
-					input: svgBuffer,
-					top: 0,
-					left: 0,
-				},
-			])
-			// this makes a image created to file.
-			.toFile(`${dir}/out.png`, () => {
-				const end = Date.now();
-				
-				try{
-					const fileImage = new AttachmentBuilder(`${dir}/out.png`);
-					const exampleEmbed = new EmbedBuilder()
-						.setColor(0x0099FF)
-						.setTitle(interaction.commandName)
-						.setDescription(`${user1}❤️${user2}`)
-						.setImage('attachment://out.png')
-						.setFooter({ text: `${interaction.member.displayName}`});
-					interaction.reply({ embeds: [exampleEmbed], files: [fileImage]});
-					console.log(`Execution time: ${end - t} ms`);
-				}catch(err){
-					console.log(err)
-					error.execute(interaction,`The bot couldn't render the image successfully pls try again`);
-				}
-			})
-	} catch (error) {
-		console.log(error);
+  
+	// Use the bisect.bisect_left() function to find the index of the element in the array that is closest to `num`.
+	const index = bisect.bisect_left(colorArray, num);
+  
+	// If the index is equal to the length of the array, then return the index - 1.
+	if (index === colorArray.length) {
+	  return index - 1;
+	} else if (num === colorArray[index]) {
+	  // If the number is equal to the element at the index, then return the index.
+	  return index;
+	} else {
+	  // Otherwise, return the index.
+	  return index;
 	}
-}
+};
+const compositeImages = async (img1, img2, backImg, dir, t, num, interaction, user1, user2) => {
+	// Random number 0 to number of images template.
+	const path = `./img/img-temple/ship/`;
+	let randomInt = null;
+	await fs.readdir(path, (err, files) => {
+	  randomInt = Math.floor(Math.random() * files.length);
+	});
+  
+	// Create a SVG image with text on it with color and variable num.
+	const svgImage = `
+	  <svg width="1024" height="512">
+		<style>
+		  .title {
+			fill: ${color[i]};
+			font-size: 100px;
+			font-family: Arial, sans-serif;
+			font-weight: normal;
+			paint-order: stroke;
+			stroke: #000000;
+			stroke-width: 8px;
+			stroke-linecap: butt;
+			stroke-linejoin: miter;
+		  }
+		</style>
+		<text x="50%" y="95%" text-anchor="middle" class="title">${num}%</text>
+	  </svg>
+	`;
+  
+	// Create a buffer from the SVG image, img1Buffer and img2Buffer.
+	const svgBuffer = Buffer.from(svgImage);
+	const img1Buffer = Buffer.from(img1, 'base64');
+	const img2Buffer = Buffer.from(img2, 'base64');
+  
+	// Resize the image size to the PositionShip[index].width.
+	img1Buffer = await sharp(img1Buffer)
+	  .resize({ width: PositionShip[randomInt].width })
+	  .toBuffer();
+	img2Buffer = await sharp(img2Buffer)
+	  .resize({ width: PositionShip[randomInt].width })
+	  .toBuffer();
+  
+	// Composite all images in the next order 1st img1Buffer, 2nd img2Buffer, 3rd image template and 4th svgBuffer.
+	const compositedImage = sharp(backImg)
+	  .composite([
+		{
+		  input: img1Buffer,
+		  top: PositionShip[randomInt].y1,
+		  left: PositionShip[randomInt].x1,
+		},
+		{
+		  input: img2Buffer,
+		  top: PositionShip[randomInt].y2,
+		  left: PositionShip[randomInt].x2,
+		},
+		{
+		  input: `./img/img-temple/ship/${randomInt + 1}.png`,
+		  top: 0,
+		  left: 0,
+		},
+		{
+		  input: svgBuffer,
+		  top: 0,
+		  left: 0,
+		},
+	  ]);
+  
+	// Save the composited image to a file.
+	await compositedImage.toFile(`${dir}/out.png`);
+  
+	// Create an Embed object and send it to Discord.
+	const fileImage = new AttachmentBuilder(`${dir}/out.png`);
+	const exampleEmbed = new EmbedBuilder()
+	  .setColor(0x0099FF)
+	  .setTitle(interaction.commandName)
+	  .setDescription(`${user1}❤️${user2}`)
+	  .setImage('attachment://out.png')
+	  .setFooter({ text: `${interaction.member.displayName}`});
+	interaction.reply({ embeds: [exampleEmbed], files: [fileImage]});
+  
+	// Log the execution time.
+	const end = Date.now();
+	console.log(`Execution time: ${end - t} ms`);
+};
 module.exports = {
 	data: new SlashCommandBuilder()
 	// info about the command like Name, Description, User Options, etc.
